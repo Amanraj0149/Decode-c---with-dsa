471f2face737ea7189ecf482a71b70d8de469729
Aman Raj <135144595+Amanraj0149@users.noreply.github.com>
2024-06-21 12:14:25 +0530

    update

--- sets/mapsBasic.cpp
+++ sets/mapsBasic.cpp
@@ -8,13 +8,28 @@ int main(){
      p1.first="aman";
      p1.second=76;
       pair<string,int>p2;
-     p2.first="harsh";
-     p2.second=77;
-      pair<string,int>p3;
-     p3.first="vijay";
-     p3.second=2;
-      pair<string,int>p4;
-     p4.first="rizz";
-     p4.second=1;
-     m.insert(p1);
+    //  p2.first="harsh";
+    //  p2.second=77;
+    //   pair<string,int>p3;
+    //  p3.first="vijay";
+    //  p3.second=2;
+    //   pair<string,int>p4;
+    //  p4.first="rizz";
+    //  p4.second=1;
+    m.insert(p1);
+    //   m.insert(p2);
+    //    m.insert(p3);
+    //     m.insert(p4);
+   m["harsh"]=15;
+   m["lokesh"]=67;
+     
+        for(auto p :m){
+            cout<<p.first<<" "<<p.second<<endl;
+        }
+        cout<<m.size()<<endl;
+        m.erase("lokesh");
+        cout<<m.size()<<endl;
+          for(auto p :m){
+            cout<<p.first<<" "<<p.second<<endl;
+        }
 }
